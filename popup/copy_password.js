fd33a33b3b4b61f3c694b8cf5dbe26aec0ad7c30
Aaron Bopp <s533127@nwmissouri.edu>
2019-10-07 14:32:02 -0500

    added clipboard funcitonality

--- popup/copy_password.js
+++ popup/copy_password.js
@@ -1,7 +1,17 @@
-document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("button")){
-      console.log("You knocked?");
-    }
+//Copies the first selector with the given ID. 
+//Using .execCommand not the clipboard API because that is all the functionality needed and it doesn't need permissions
+function copy(id) {
+  let copyText = document.querySelector("#" + id);
+  copyText.select();
+  document.execCommand("copy");
+}
 
+//Main click handler
+document.addEventListener("click", (e) => {
+
+    //If you click on copy boxes copies the contents to the clipboard
+    if (e.target.classList.contains("copy")){
+      copy(e.target.id)
+    }
     
   });
