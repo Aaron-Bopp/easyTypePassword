4a175ebbe99db78d933d22879656ea9ef5b0136e
Aaron Bopp <s533127@nwmissouri.edu>
2019-10-12 18:30:36 -0500

    input boxes now populate from the js

--- popup/copy_password.js
+++ popup/copy_password.js
@@ -5,13 +5,21 @@ function copy(id) {
   //Using .execCommand not the clipboard API because that is all the functionality needed and it doesn't need permissions 
   document.execCommand("copy");
 }
-
+//TODO: implement password generator
+function genPass(criteria){
+  
+}
+//TODO: implement support for a variable amount of input boxes
+document.getElementById("input1").value = "yup";
+document.getElementById("input2").value = "yup";
+document.getElementById("input3").value = "yup";
 //Main click handler
 document.addEventListener("click", (e) => {
 
     //If you click on copy boxes copies the contents to the clipboard
     if (e.target.classList.contains("copy")){
-      copy(e.target.id)
+      copy(e.target.id);
+
     }
     
   });
